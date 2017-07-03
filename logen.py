24aaf3a7c7c71597086c8b3742458685f8312229
Mario Alina <Mario.alina11@gmail.com>
2017-07-03 14:49:57 +0200

    Add files via upload

---
+++ logen.py
@@ -0,0 +1,32 @@
+import requests 
+from BeautifulSoup import *
+
+
+r1 = requests.get('http://ts2.travian.sk/login.php')
+unicodeData = r1.text
+unicodeData.encode('ascii', 'ignore')
+soup = BeautifulSoup(unicodeData)
+tags = soup('input')
+value_list = []
+
+for tag in tags:
+    value_list.append(tag.get('value', None))
+
+my_id = value_list[4]
+
+payload = {'login': my_id, 
+           'name': 'Ashreen', 
+           'password': 'testing', 
+           's1': 'Login', 
+           'w': '1920:1080'}
+session = requests.Session()
+r2 = session.post('http://ts2.travian.sk/dorf1.php', data=payload, cookies=r1.cookies)
+
+unicodeData = r2.text
+my_soup = BeautifulSoup(unicodeData)
+print type(unicodeData)
+print type(my_soup)
+
+file = open("testfile.txt","w")
+file.write(str(my_soup))
+file.close()
