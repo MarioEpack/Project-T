aa84cec7d39d8154a1e7e9a3ec3c4461b2521a1f
Mario Alina <Mario.alina11@gmail.com>
2017-07-07 14:15:47 +0200

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,8 +1,37 @@
 import re
 from bs4 import BeautifulSoup
+import sqlite3
+
+#SQLITE code
+conn = sqlite3.connect('travdate.sqlite')
+cur = conn.cursor()
+
+cur.executescript('''
+DROP TABLE IF EXISTS resources;
+DROP TABLE IF EXISTS storage;
+
+CREATE TABLE resources (
+    village_id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    lumber       INTEGER NOT NULL,
+    clay         INTEGER NOT NULL,
+    iron         INTEGER NOT NULL,
+    crop         INTEGER NOT NULL,
+    lumber_prod  INTEGER NOT NULL,
+    clay_prod    INTEGER NOT NULL,
+    iron_prod    INTEGER NOT NULL,
+    crop_prod    INTEGER NOT NULL
+);
+
+CREATE TABLE storage (
+    village_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    warehouse   INTEGER NOT NULL,
+    grannary    INTEGER NOT NULL
+
+)
+''')
+
+
 #Scraping the updates
-
-
 #UPDATE STORAGE FUNCTIONS
 
 def update_lumber_stock(my_soup):
@@ -10,40 +39,40 @@ def update_lumber_stock(my_soup):
     value_re = re.findall(r'\d', str(lumber_storage))
     value_re.remove(value_re[0])
     value = ''.join(value_re)
-    return value
+    return value + ' lumber stock.'
 
 def update_clay_stock(my_soup):
     clay_storage = my_soup.find(id= "l2")
     value_re = re.findall(r'\d', str(clay_storage))
     value_re.remove(value_re[0])
     value = ''.join(value_re)
-    return value
+    return value + ' clay stock.'
 
 def update_iron_stock(my_soup):
     iron_storage = my_soup.find(id= "l3")
     value_re = re.findall(r'\d', str(iron_storage))
     value_re.remove(value_re[0])
     value = ''.join(value_re)
-    return value
+    return value + ' iron stock.'
 
 def update_crop_stock(my_soup):
     crop_storage = my_soup.find(id= "l4")
     value_re = re.findall(r'\d', str(crop_storage))
     value_re.remove(value_re[0])
     value = ''.join(value_re)
-    return value
+    return value + ' crop stock.'
 
 def update_warehouse(my_soup):
     warehouse_storage = my_soup.find(id="stockBarWarehouse")
     value_re = re.findall(r'\d', str(warehouse_storage))
     value = ''.join(value_re)
-    return value
+    return value + ' current warehouse stock.'
 
 def update_granary(my_soup):
     granary_storage = my_soup.find(id="stockBarGranary")
     value_re = re.findall(r'\d', str(granary_storage))
     value = ''.join(value_re)
-    return value
+    return value + ' current granary stock.'
 
 #UPDATE PRODUCTION FUNCTIONS       
 
@@ -52,26 +81,26 @@ def update_lumber_prod(my_soup):
     lumber_prod = re.findall(r'\d', str(soup_lumber_prod))
     lumber_prod.remove(lumber_prod[0])
     lumber = ''.join(lumber_prod)
-    return lumber
+    return lumber + ' lumber production'
 
 def update_clay_prod(my_soup):
     soup_clay_prod = my_soup.find(href="production.php?t=2")
     clay_prod = re.findall(r'\d', str(soup_clay_prod))
     clay_prod.remove(clay_prod[0])
     clay = ''.join(clay_prod)
-    return clay
+    return clay + ' clay production'
 
 def update_iron_prod(my_soup):
     soup_iron_prod = my_soup.find(href="production.php?t=3")
     iron_prod = re.findall(r'\d', str(soup_iron_prod))
     iron_prod.remove(iron_prod[0])
     iron = ''.join(iron_prod)
-    return iron
+    return iron + ' iron production'
 
 def update_crop_prod(my_soup):
     soup_crop_prod = my_soup.find(href="production.php?t=5")
     crop_prod = re.findall(r'\d+', str(soup_crop_prod))
-    return crop_prod[3]
+    return crop_prod[3] + ' crop production'
 
 def total_update():
     print update_lumber_stock(my_soup)
@@ -85,5 +114,20 @@ def total_update():
     print update_clay_prod(my_soup)
     print update_iron_prod(my_soup)
     print update_crop_prod(my_soup)
+
+
+#SQLITE execute
+
+cur.execute('''INSERT INTO resources(lumber, clay, iron, crop, lumber_prod,
+clay_prod, iron_prod, crop_prod) VALUES (?, ?, ?, ?, ?, ?, ?)''',
+(update_lumber_stock(), update_clay_stock(), update_iron_stock(), 
+    update_crop_stock(), update_lumber_prod(),update_clay_prod(),
+    update_iron_prod(), update_crop_prod())
+
+cur.execute('''INSERT INTO storage(warehouse, grannary) VALUES(?, ?)''',
+    (update_warehouse(), update_granary())
+
+
+conn.commit()
 
 
