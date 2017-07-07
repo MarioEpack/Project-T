20d066aaee267baa1e83c462a792e3e8fd320fae
Mario Alina <Mario.alina11@gmail.com>
2017-07-07 14:20:15 +0200

    Update updates.py

--- updates.py
+++ updates.py
@@ -39,40 +39,40 @@ def update_lumber_stock(my_soup):
     value_re = re.findall(r'\d', str(lumber_storage))
     value_re.remove(value_re[0])
     value = ''.join(value_re)
-    return value + ' lumber stock.'
+    return value
 
 def update_clay_stock(my_soup):
     clay_storage = my_soup.find(id= "l2")
     value_re = re.findall(r'\d', str(clay_storage))
     value_re.remove(value_re[0])
     value = ''.join(value_re)
-    return value + ' clay stock.'
+    return value
 
 def update_iron_stock(my_soup):
     iron_storage = my_soup.find(id= "l3")
     value_re = re.findall(r'\d', str(iron_storage))
     value_re.remove(value_re[0])
     value = ''.join(value_re)
-    return value + ' iron stock.'
+    return value
 
 def update_crop_stock(my_soup):
     crop_storage = my_soup.find(id= "l4")
     value_re = re.findall(r'\d', str(crop_storage))
     value_re.remove(value_re[0])
     value = ''.join(value_re)
-    return value + ' crop stock.'
+    return value
 
 def update_warehouse(my_soup):
     warehouse_storage = my_soup.find(id="stockBarWarehouse")
     value_re = re.findall(r'\d', str(warehouse_storage))
     value = ''.join(value_re)
-    return value + ' current warehouse stock.'
+    return value
 
 def update_granary(my_soup):
     granary_storage = my_soup.find(id="stockBarGranary")
     value_re = re.findall(r'\d', str(granary_storage))
     value = ''.join(value_re)
-    return value + ' current granary stock.'
+    return value
 
 #UPDATE PRODUCTION FUNCTIONS       
 
@@ -81,26 +81,26 @@ def update_lumber_prod(my_soup):
     lumber_prod = re.findall(r'\d', str(soup_lumber_prod))
     lumber_prod.remove(lumber_prod[0])
     lumber = ''.join(lumber_prod)
-    return lumber + ' lumber production'
+    return lumber
 
 def update_clay_prod(my_soup):
     soup_clay_prod = my_soup.find(href="production.php?t=2")
     clay_prod = re.findall(r'\d', str(soup_clay_prod))
     clay_prod.remove(clay_prod[0])
     clay = ''.join(clay_prod)
-    return clay + ' clay production'
+    return clay
 
 def update_iron_prod(my_soup):
     soup_iron_prod = my_soup.find(href="production.php?t=3")
     iron_prod = re.findall(r'\d', str(soup_iron_prod))
     iron_prod.remove(iron_prod[0])
     iron = ''.join(iron_prod)
-    return iron + ' iron production'
+    return iron
 
 def update_crop_prod(my_soup):
     soup_crop_prod = my_soup.find(href="production.php?t=5")
     crop_prod = re.findall(r'\d+', str(soup_crop_prod))
-    return crop_prod[3] + ' crop production'
+    return crop_prod[3]
 
 def total_update():
     print update_lumber_stock(my_soup)
@@ -122,12 +122,13 @@ cur.execute('''INSERT INTO resources(lumber, clay, iron, crop, lumber_prod,
 clay_prod, iron_prod, crop_prod) VALUES (?, ?, ?, ?, ?, ?, ?)''',
 (update_lumber_stock(), update_clay_stock(), update_iron_stock(), 
     update_crop_stock(), update_lumber_prod(),update_clay_prod(),
-    update_iron_prod(), update_crop_prod())
+    update_iron_prod(), update_crop_prod()))
 
 cur.execute('''INSERT INTO storage(warehouse, grannary) VALUES(?, ?)''',
-    (update_warehouse(), update_granary())
+    (update_warehouse(), update_granary()))
 
 
 conn.commit()
+
 
 
